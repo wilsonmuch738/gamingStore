f1e848542adca360bc82ed7017e332751d2fe444
Fernando Vicente Jauya Diaz <fernandojauya30@gmail.com>
2023-10-23 12:56:14 -0500

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,43 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p>
+                    Somos una empresa de venta de todo tipo de artefactos a un buen precio y con muchos descuentos
+                    dentro de nuestros productos.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Direccion</h4>
+                <p style="color: #FFF">La Paz-Bolivia</p>
+                <p style="font-size:20px" aria-hidden="true"> +591 – 77706721</p>
+                <p class="fa fa-mobile" style="font-size:20px" aria-hidden="true"> 77706721 / 65667888</p> <a href="#"
+                    target="_blank" style="color: #5bc0de">envianos_un_mensaje</a></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; ilaluquelopezmanuel@gmail.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">STORE &copy; 2022</h5>
+</footer>
